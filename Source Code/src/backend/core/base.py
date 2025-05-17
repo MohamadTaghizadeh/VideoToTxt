a9fc5cfd1b61d9b1cd949606b8c16bb5ec263079
Mohamad Taghizadeh <81808969+MohamadTaghizadeh@users.noreply.github.com>
2025-05-17 10:19:48 +0330

    Update base.py

--- Source Code/src/backend/core/base.py
+++ Source Code/src/backend/core/base.py
@@ -1,1 +1,21 @@
+import os
+from dbutils import models
+from dbutils.database import SessionLocal, engine
+from config.config_handler import config
 
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class Base:
+    def __init__(self):
+        if not os.path.exists(config["DB_DIR"]):
+            os.makedirs(config["DB_DIR"])
