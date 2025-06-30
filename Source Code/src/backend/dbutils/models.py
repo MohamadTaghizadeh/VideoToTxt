d3a4b1cd69225e26b881c0f72c3ec3a04639f644
Mohamad Taghizadeh <81808969+MohamadTaghizadeh@users.noreply.github.com>
2025-06-30 12:54:52 +0330

    Update models.py

--- Source Code/src/backend/dbutils/models.py
+++ Source Code/src/backend/dbutils/models.py
@@ -1,1 +1,25 @@
+from sqlalchemy import Column, String, DateTime, Integer, Enum, SmallInteger, JSON
+from dbutils.database import Base
+from core.utils import generate_uuid
+from dbutils.schemas import WebhookStatus
+
+
+def str(self) -> str:
+    return " , ".join([f"{key}: {value}" for key, value in self.__dict__.items()][::-1])
+
+
+def items(self) -> dict:
+    result = {}
+    for k, v in self.__dict__.items():
+        if not k.startswith("_"):
+            result[k] = v
+    return result.items()
+
+
+Base.__str__ = str
+Base.items = items
+
+
+# Base.__repr__ = str
+
 
